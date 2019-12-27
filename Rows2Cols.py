985dfc0fa3eecc272ee85473d4e9ad966ed86c48
Helly <41030673+HellyJain@users.noreply.github.com>
2019-12-26 20:14:10 -0500

    Update and rename Rows2Cols to Rows2Cols.py

--- Rows2Cols.py
+++ Rows2Cols.py
@@ -16,15 +16,3 @@ def rowstocols(df, key, value, join_how='outer'):
         df_v = df_v.rename(columns = {value:c})       
         tibble = tibble.merge(df_v, on=list(fixed_vars), how=join_how)
     return tibble
-
-def melt(df, col_vals, key, value):
-    assert type(df) is pd.DataFrame
-    keep_vars = df.columns.difference(col_vals)
-    melted_sections = []
-    for c in col_vals:
-        melted_c = df[keep_vars].copy()
-        melted_c[key] = c
-        melted_c[value] = df[c]
-        melted_sections.append(melted_c)
-    melted = pd.concat(melted_sections)
-    return melted
